cdeb70fa25948feae33008b9c58c6c4fd7826541
mohithambore <mohithambore.s@gmail.com>
2026-06-01 20:30:33 +0530

    Fix userdata for Ubuntu

--- terraform/userdata.sh
+++ terraform/userdata.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
-yum update -y
+apt update -y
 
-yum install -y python3 git
+apt install -y git python3 python3-pip curl
 
-curl -fsSL https://rpm.nodesource.com/setup_18.x | bash -
+curl -fsSL https://deb.nodesource.com/setup_18.x | bash -
+apt install -y nodejs
 
-yum install -y nodejs
+cd /home/ubuntu
 
-echo "Terraform setup completed" > /home/ec2-user/setup.log
+git clone https://github.com/mohithambore/Assignment8_Terraform_AWS.git
+
+cd /home/ubuntu/Assignment8_Terraform_AWS/flask-backend
+
+pip3 install -r requirements.txt
+
+nohup python3 app.py > flask.log 2>&1 &
+
+cd /home/ubuntu/Assignment8_Terraform_AWS/express-frontend
+
+npm install
+
+nohup node app.js > express.log 2>&1 &
